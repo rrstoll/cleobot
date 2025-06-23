926b352f7a092a59053691af39512d62a71b785b
Russell Stoll <rrstoll@users.noreply.github.com>
2025-06-23 13:50:58 -0400

    Add files via upload

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    color: {
+      'myPapyrus': '#fdfde3'
+    },
+    extend: {
+      backgroundImage: {
+        'bg-papyrus': "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAARVBMVEUAAAD////h4eHBwcHHx8fLy8vV1dXn5+fat7fq6urq6urp6enp6enq6urq6urp6enr6+vq6urq6uromy0dAAAADXRSTlMAEBAgIDM0QEFHT05R4vHxAAAAUUlEQVQYlWNgwA0EBjYmBjBgYGBgYmBgYGCZAQYGB4Z3CIgUICAiICDwIqwMDQYJDKAcPAmAluSFIAqwopsQwdQBiqAgAAMsWBNn2HwYkAAAAABJRU5ErkJggg==')",
+      },
+      fontFamily: {
+        papyrus: ['Papyrus', 'cursive'],
+      },
+    },
   },
   plugins: [],
 };
