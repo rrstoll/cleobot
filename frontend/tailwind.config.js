0c6f77964ec19c0f294bd04e4fd74b55d55340ba
Russell Stoll <rrstoll@users.noreply.github.com>
2025-06-23 13:58:05 -0400

    Add files via upload

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
     color: {
-      'myPapyrus': '#fdfde3'
+      'bgPapyrus': '#fdfde3'
     },
     extend: {
       backgroundImage: {
