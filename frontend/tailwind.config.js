441502a4a542338b66db9d065f0b741387806263
Russell Stoll <rrstoll@users.noreply.github.com>
2025-06-24 14:54:08 -0400

    Add files via upload

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
       colors: {
         colorLtTan: "#dee1dd",
         colorTan: "#c4cdc1",
-        colorLtGreen: "# 99aead",
+        colorLtGreen: "#99aead",
         colorBlue: "#6d9197",
         colorGreen: "#658b6f",
         colorDkGreen: "#2f575d",
