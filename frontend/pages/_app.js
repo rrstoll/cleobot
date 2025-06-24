d40f514ea8ada4622a70568e6d53f4e1d9f00fee
Russell Stoll <rrstoll@users.noreply.github.com>
2025-06-24 17:36:19 -0400

    Add files via upload

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
