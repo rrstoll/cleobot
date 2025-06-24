fba1a7553927e69cc796816a071b05fa3b250528
Russell Stoll <rrstoll@users.noreply.github.com>
2025-06-24 10:50:23 -0400

    Add files via upload

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
       backgroundImage: {
         bgPapyrus: "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAARVBMVEUAAAD////h4eHBwcHHx8fLy8vV1dXn5+fat7fq6urq6urp6enp6enq6urq6urp6enr6+vq6urq6uromy0dAAAADXRSTlMAEBAgIDM0QEFHT05R4vHxAAAAUUlEQVQYlWNgwA0EBjYmBjBgYGBgYmBgYGCZAQYGB4Z3CIgUICAiICDwIqwMDQYJDKAcPAmAluSFIAqwopsQwdQBiqAgAAMsWBNn2HwYkAAAAABJRU5ErkJggg==')",
       },
+      fontFamily: {
+        serif: ['"EB Garamond"', 'serif'],
+      },
     },
   },
   plugins: [],
